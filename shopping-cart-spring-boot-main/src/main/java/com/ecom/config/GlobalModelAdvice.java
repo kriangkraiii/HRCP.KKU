eccd2073a3e37ad0eb547de2b51b64288025fc0e
kriangkraiii <kriangkrai.p@kkumail.com>
2026-02-11 15:02:55 +0700

    Update GlobalModelAdvice.java

--- shopping-cart-spring-boot-main/src/main/java/com/ecom/config/GlobalModelAdvice.java
+++ shopping-cart-spring-boot-main/src/main/java/com/ecom/config/GlobalModelAdvice.java
@@ -1,16 +1,34 @@
 package com.ecom.config;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.ecom.model.UserDtls;
+import com.ecom.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
 @ControllerAdvice
 public class GlobalModelAdvice {
 
+    @Autowired
+    private UserService userService;
+
     @ModelAttribute
-    public void transferSessionMessages(HttpSession session, Model model) {
+    public void addGlobalAttributes(Principal principal, HttpSession session, Model model) {
+        // Add logged-in user to model for ALL controllers
+        if (principal != null) {
+            UserDtls user = userService.getUserByEmail(principal.getName());
+            if (user != null) {
+                model.addAttribute("user", user);
+            }
+        }
+
+        // Transfer session messages to model and clear them
         String succMsg = (String) session.getAttribute("succMsg");
         String errorMsg = (String) session.getAttribute("errorMsg");
 
